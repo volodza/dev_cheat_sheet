ecaea2b8f67aba95796d63c9fcb20a3f1f3eb4e9
Volodza <looka66@mail.ru>
2019-12-09 19:23:47 +0300

    index and db

---
+++ server/index.js
@@ -0,0 +1,25 @@
+const Express = require('express');
+const Cors = require('cors');
+const BodyParser = require('body-parser');
+const db = require('./db')
+const app = Express();
+const port = process.env.PORT || 3000;
+
+app.use(Cors());
+app.use(BodyParser.json());
+app.use(Express.static(__dirname + '/public/'))
+app.get(/.*/, (req,res)=> res.sendFile(__dirname + '/public/index.html'))
+
+db.connect((err)=>{
+  if (err) {
+    return console.log(err);
+    process.exit(1)
+  } else {
+    app.listen(port, (err) => {
+      if (err) {
+          return console.log('something bad happened', err)
+      }
+      console.log(`server is listening on ${port}`)
+    })
+  }
+})
